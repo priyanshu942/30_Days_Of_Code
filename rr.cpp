8c27a9088f1d2423eec09409c8c0a2362806fad0
priyanshu942 <103110168+priyanshu942@users.noreply.github.com>
2023-08-08 13:11:39 +0530

    rr rotaion of array optimised

--- rr.cpp
+++ rr.cpp
@@ -7,8 +7,8 @@ int main()
 	cout<<"Enter the number of elements\n";
 	cin>>n;
 	int arr[1000];
+	int arr1[1000]={0};
 	cout<<"Enter the elements\n";
-	
 	for(int i=0;i<n;i++)
 	{
 		cin>>arr[i];
@@ -16,6 +16,12 @@ int main()
 	int r;
 	cout<<"Enter to be roated by\n";
 	cin>>r;
+	int ar=n-r;
+	for(int i=0;i<n;i++)
+	{
+		arr1[(i+ar)%(n)]=arr[i];
+	}
+
 	
 //	for(int i=0;i<r;i++)
 //	{
@@ -30,7 +36,7 @@ int main()
 
 	for(int i=0;i<n;i++)
 	{
-		cout<<arr[i]<<" ";
+		cout<<arr1[i]<<" ";
 	}
 	
 	return 0;
