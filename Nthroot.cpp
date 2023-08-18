025dfe5945245fea467137e022a6051cc0a8c6d1
priyanshu942 <103110168+priyanshu942@users.noreply.github.com>
2023-08-18 23:54:45 +0530

    Nth root

---
+++ Nthroot.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	int m;
+	cout<<"You have to find root of\n";
+	cin>>n;
+	cout<<"You have to find which root\n";
+	cin>>m;
+	int flag=0;
+	int a=1;
+	while(a=<n)
+	{
+		a=a*m;
+	}
+	
+	
+	
+	return 0;
+}
